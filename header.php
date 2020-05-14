49722306d8c1e40c6054214b4a01c90c196765b2
amandeepsharma28 <57304688+amandeepsharma28@users.noreply.github.com>
2020-05-14 01:29:39 -0400

    initial commit

---
+++ header.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html ln="en">
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Playball&display=swap" rel="stylesheet">
+    <title>Soulmate</title>
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+</head>
+
+<body>
+    <div class="grid-container">
+        <header class="header">
+            <h1>S<img src="images/heart.png" alt="heart" />ulmate</h1>
+            <nav><a href="login.php" id="login">Login</a></nav>
+            <form action="search.php" target="iframe_a" method="post">
+                <input type="text" name="search" value="Enter Name"/>
+                <input type="submit" value="Search"/>
+            </form>
+        </header>
